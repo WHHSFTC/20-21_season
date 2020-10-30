fbd36d9a2f3bbfde108e468466cab58adb511c3b
JarnaChao09 <yunyang4928@gmail.com>
2020-10-30 17:22:59 -0400

    add rest of tuning opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/FollowerPIDTuner.kt
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.test
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.firstinspires.ftc.teamcode.module.CustomMecanumDrive
+import org.firstinspires.ftc.teamcode.module.OpMode
+
+@Autonomous(group = "drive")
+class FollowerPIDTuner : OpMode(Mode.TELE) {
+    private lateinit var drive: CustomMecanumDrive
+    private lateinit var startPose: Pose2d
+
+    override fun onInit() {
+        drive = CustomMecanumDrive(bot)
+    }
+
+    override fun onRun() {
+        startPose = Pose2d(-DISTANCE / 2, -DISTANCE / 2, 0.0)
+        drive.poseEstimate = startPose
+    }
+
+    override fun onLoop() {
+        val traj = drive.trajectoryBuilder(startPose)
+                .forward(DISTANCE)
+                .build()
+        drive.followTrajectory(traj)
+        drive.turn(Math.toRadians(90.0))
+        startPose = traj.end().plus(Pose2d(0.0, 0.0, Math.toRadians(90.0)))
+    }
+
+    override fun onStop() {}
+
+    companion object {
+        var DISTANCE = 48.0 // in
+    }
+}

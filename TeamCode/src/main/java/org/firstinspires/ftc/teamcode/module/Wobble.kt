fc09e3e0c0263a440b5f660896860a4fbb763934
Luke Early <23438839+lukeearly@users.noreply.github.com>
2020-12-26 11:08:36 -0500

    wobble positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Wobble.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/module/Wobble.kt
@@ -6,7 +6,7 @@ class Wobble(val bot: Robot) {
     }
 
     enum class ClawState(override val pos: Double): StatefulServo.ServoPosition {
-        OPEN(.49), CLOSED(.18)
+        OPEN(.2), CLOSED(.5)
     }
 
     var elbow = StatefulServo<ElbowState>(bot.hwmap.servo["elbow"], ElbowState.STORE)
